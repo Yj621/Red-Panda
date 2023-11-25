0c3f677a7361cafc43c6bd12f39c4a7484fe8977
jieun <122079153+Cjieun@users.noreply.github.com>
2023-11-26 00:40:15 +0900

    mainPage UI

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,11 +1,29 @@
 import DropdownBox from "../../components/dropdown/DropdownBox";
 import MainBox from "../../components/mainBox/MainBox";
+import "./Main.css";
 
 export default function Main() {
   return (
-    <div>
-      <DropdownBox />
-      <MainBox />
+    <div className="mainPageLayout">
+      <div className="mainPageContainer">
+        <div className="mainPageDropdown">
+          <DropdownBox />
+        </div>
+        <div className="mainPageBox">
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+          <MainBox />
+        </div>
+      </div>
     </div>
   );
 }
