197b3897f3f938f86e47f5419f1df64a43833619
LeeKangHoo <protection0828@gmail.com>
2023-11-26 17:36:54 +0900

    title,content input

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,42 +1,24 @@
+/*eslint-disable */
 import React, { useState } from 'react';
+import './Write.css';
 
 function PostForm() {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // 여기서 데이터를 처리하거나 API로 전송하는 등의 로직을 추가할 수 있습니다.
-    console.log('제목:', title);
-    console.log('내용:', content);
-    // 데이터 처리 후, 필요한 동작을 수행합니다.
-  };
+
+  let titleDefault = '제목을 입력하세요.';
 
   return (
-    <div>
-      <h1>게시글 작성하기</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">제목:</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+    <div className='writemain'>
+
+        <div className='title'>
+          <input type='text' placeholder='제목을 입력하세요' />
         </div>
-        <div>
-          <label htmlFor="content">내용:</label>
-          <textarea
-            id="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+        <div className='content'>
+          <input type='text' placeholder='내용을 입력하세요' />
         </div>
-        <button type="submit">게시글 작성</button>
-      </form>
-    </div>
+      </div>
   );
 }
+
 
 export default PostForm;
