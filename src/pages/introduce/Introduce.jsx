5a75d5fa3f13de584887a4bac833231898d9edd2
Yj621 <wjddbswl26@gmail.com>
2023-11-27 11:27:15 +0900

    Introduce
    Introduce

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,37 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import teamLogo from "../../images/teamlogo.jpeg";
+import yj from "../../images/yj.jpeg";
+import je from "../../images/je.png";
 import "./Introduce.css";
 
 function Introduce() {
   const [show, setShow] = useState(false);
-  const [scroll, setScroll] = useState(false);
-
-  useEffect(() => {
-    const onScroll = () => {
-      const isScroll = window.scrollY > 20;
-      setScroll(isScroll);
-    };
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  const centerStyle = scroll
-    ? {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        height: "200vh",
-        overflow: "scroll",
-      }
-    : {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "150vh",
-        overflow: "scroll",
-      };
+  const [showYj, setShowYj] = useState(false);
+  const [showJe, setShowJe] = useState(false);
 
   const buttonStyle = {
     background: "none",
@@ -40,30 +16,61 @@ function Introduce() {
     font: "inherit",
     cursor: "pointer",
     outline: "inherit",
+    display: show ? "none" : "block",
   };
 
-  const showStyle = {
-    animation: show ? "slide-up 1s forwards" : "",
+  const textContainerStyle = {
+    position: "absolute",
+    top: "20px",
+    left: "20px",
     opacity: show ? 1 : 0,
+    transition: "opacity 0.5s ease-in",
+    paddingLeft: "50px"
   };
+
+  const imgStyle ={
+    width: "200px", 
+    height: "200px", 
+    borderRadius: "70%",
+    opacity: showYj || showJe ? 1 : 0,
+    transition: "opacity 0.5s ease-in"
+  }
 
   const handleButtonClick = () => {
     setShow(true);
+    setTimeout(() => {
+      setShowYj(true);
+    }, 1000);
+    setTimeout(() => {
+      setShowJe(true);
+    }, 2000);
   };
 
   return (
-    <div style={centerStyle}>
-      {scroll && <p style={{ position: "fixed", top: 40 }}>팀 소개</p>}
-      {show ? (
-        <div style={showStyle}>
-          <p style={{ textAlign: scroll ? "left" : "center" }}>레서판다팀</p>
-          <img src={teamLogo} alt="team logo" />
-        </div>
-      ) : (
-        <button style={buttonStyle} onClick={handleButtonClick}>
-          팀 소개 보기
-        </button>
-      )}
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", position: "relative" }}>
+      <button style={buttonStyle} onClick={handleButtonClick}>
+        팀 소개 보기
+      </button>
+      <div style={textContainerStyle}>
+        <p style={{ fontSize: "40px", margin: "0px" }}>팀 소개</p>
+        <p>레서판다팀</p>
+        <img src={teamLogo} alt="team logo" style={{ width: "200px", height: "150px" }} />
+
+        {showYj && (
+          <div>
+            <img src={yj} alt="yj" style={imgStyle} />
+          </div>
+        )}
+
+        {showJe && (
+          <div>
+            <hr />
+            <img src={je} alt="je" style={imgStyle} />
+          </div>
+        )}
+      </div>
+      
+      <br />
     </div>
   );
 }
