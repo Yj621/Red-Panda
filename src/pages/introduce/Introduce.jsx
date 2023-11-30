77c2c646af755938b932252bfcac091ea6ba0ca3
Yj621 <wjddbswl26@gmail.com>
2023-11-30 12:20:24 +0900

    FEAT : Introduce
    FEAT : Introduce

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import teamLogo from "../../images/teamlogo.jpeg";
 import yj from "../../images/yj.jpeg";
 import je from "../../images/je.png";
@@ -6,8 +6,9 @@ import "./Introduce.css";
 
 function Introduce() {
   const [show, setShow] = useState(false);
-  const [showYj, setShowYj] = useState(false);
   const [showJe, setShowJe] = useState(false);
+  const [opacityYj, setOpacityYj] = useState(0);
+  const [opacityJe, setOpacityJe] = useState(0);
 
   const buttonStyle = {
     background: "none",
@@ -25,51 +26,70 @@ function Introduce() {
     left: "20px",
     opacity: show ? 1 : 0,
     transition: "opacity 0.5s ease-in",
-    paddingLeft: "50px"
+    paddingLeft: "50px",
   };
 
-  const imgStyle ={
-    width: "200px", 
-    height: "200px", 
+  const imgStyle = {
+    width: "200px",
+    height: "200px",
     borderRadius: "70%",
-    opacity: showYj || showJe ? 1 : 0,
-    transition: "opacity 0.5s ease-in"
-  }
+    transition: "opacity 0.5s ease-in",
+  };
 
   const handleButtonClick = () => {
     setShow(true);
-    setTimeout(() => {
-      setShowYj(true);
-    }, 1000);
-    setTimeout(() => {
-      setShowJe(true);
-    }, 2000);
   };
+
+  useEffect(() => {
+    const showImages = () => {
+      setTimeout(() => {
+        setOpacityYj(1);
+      }, 1000);
+      setTimeout(() => {
+        setShowJe(true);
+      }, 2000);
+    };
+  
+    if (show) {
+      showImages();
+    }
+  }, [show]);
+  
+  useEffect(() => {
+    if (showJe) {
+      setTimeout(() => {
+        setOpacityJe(1);
+      }, 1000);
+    }
+  }, [showJe]);
 
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", position: "relative" }}>
       <button style={buttonStyle} onClick={handleButtonClick}>
         팀 소개 보기
       </button>
-      <div style={textContainerStyle}>
-        <p style={{ fontSize: "40px", margin: "0px" }}>팀 소개</p>
-        <p>레서판다팀</p>
-        <img src={teamLogo} alt="team logo" style={{ width: "200px", height: "150px" }} />
+      <div>
+        <div style={textContainerStyle}>
+          <p style={{ fontSize: "40px", margin: "0px" }}>팀 소개</p>
+          <p>레서판다팀</p>
+          <img src={teamLogo} alt="team logo" style={{ width: "200px", height: "150px" }} />
+        </div>
 
-        {showYj && (
-          <div>
-            <img src={yj} alt="yj" style={imgStyle} />
-          </div>
-        )}
-
-        {showJe && (
-          <div>
-            <hr />
-            <img src={je} alt="je" style={imgStyle} />
-          </div>
-        )}
+        <div>
+          {show && (
+            <div>
+              <img src={yj} alt="yj" style={{ ...imgStyle, opacity: opacityYj }} />
+              {showJe && (
+                <div>
+                  <hr />
+                  <img src={je} alt="je" style={{ ...imgStyle, opacity: opacityJe }} />
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      
+
       <br />
     </div>
   );
