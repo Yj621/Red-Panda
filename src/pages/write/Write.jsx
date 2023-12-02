3e1b93e65874611364e86b8645644d8d29fa0ebf
LeeKangHoo <protection0828@gmail.com>
2023-12-03 03:13:01 +0900

    to db

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,21 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './Write.css';
 
 function Write() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [imageUrls, setImageUrls] = useState(['']); // 이미지 URL을 담을 배열
+
+  const handleAddImageInput = () => {
+    setImageUrls([...imageUrls, '']); // 이미지 URL 입력 칸 추가
+  };
+
+  const handleRemoveImageInput = (index) => {
+    const newImageUrls = [...imageUrls];
+    newImageUrls.splice(index, 1); // 해당 인덱스의 이미지 URL 입력 칸 제거
+    setImageUrls(newImageUrls);
+  };
+
+  const handleImageInputChange = (index, e) => {
+    const newImageUrls = [...imageUrls];
+    newImageUrls[index] = e.target.value; // 해당 인덱스의 이미지 URL 업데이트
+    setImageUrls(newImageUrls);
+  };
 
   const addNewData = async () => {
     try {
       const response = await axios.post('http://localhost:4000/contents', {
         title,
         content,
+        imageUrl: imageUrls.filter((url) => url.trim() !== ''), // 비어있는 이미지 URL 제외
       });
 
       console.log('New data added:', response.data);
       setTitle('');
       setContent('');
+      setImageUrls(['']); // 이미지 URL 입력 칸 초기화
     } catch (error) {
       console.error('Error adding data:', error);
     }
@@ -38,6 +57,20 @@ function Write() {
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
+      </div>
+      <div className='imageUrls'>
+        {imageUrls.map((url, index) => (
+          <div key={index} className='imageUrl'>
+            <input
+              type='text'
+              placeholder={`이미지 URL ${index + 1}`}
+              value={url}
+              onChange={(e) => handleImageInputChange(index, e)}
+            />
+            <button onClick={() => handleRemoveImageInput(index)}>삭제</button>
+          </div>
+        ))}
+        <button onClick={handleAddImageInput} className='addbtn'>추가</button>
       </div>
       <button onClick={addNewData}>Add Data</button>
     </div>
