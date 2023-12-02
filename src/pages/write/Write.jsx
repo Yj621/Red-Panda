e5b3a561b6866121a9bc98e63e1e2f46b86e3f0e
LeeKangHoo <protection0828@gmail.com>
2023-12-03 02:11:35 +0900

    test write page

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,24 +1,47 @@
-/*eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Write.css';
 
 function Write() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
+  const addNewData = async () => {
+    try {
+      const response = await axios.post('http://localhost:4000/contents', {
+        title,
+        content,
+      });
 
-  let titleDefault = '제목을 입력하세요.';
+      console.log('New data added:', response.data);
+      setTitle('');
+      setContent('');
+    } catch (error) {
+      console.error('Error adding data:', error);
+    }
+  };
 
   return (
     <div className='writemain'>
-
-        <div className='title'>
-          <input type='text' placeholder='제목을 입력하세요' />
-        </div>
-        <div className='content'>
-          <input type='text' placeholder='내용을 입력하세요' />
-        </div>
+      <div className='title'>
+        <input
+          type='text'
+          placeholder='제목을 입력하세요'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
+      <div className='content'>
+        <input
+          type='text'
+          placeholder='내용을 입력하세요'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </div>
+      <button onClick={addNewData}>Add Data</button>
+    </div>
   );
 }
-
 
 export default Write;
